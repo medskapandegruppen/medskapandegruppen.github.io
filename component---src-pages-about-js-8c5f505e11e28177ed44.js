493c596bc1e40c38618df5bc1e51ea0088df6347
Patrik Opacic <opacic@gmail.com>
2019-10-28 19:15:38 +0100

    clear

--- component---src-pages-about-js-8c5f505e11e28177ed44.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"3XHS":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),r=n.n(a),c=n("LbRr");t.default=function(){return r.a.createElement("div",{style:{color:"teal"}},r.a.createElement(c.a,{headerText:"GRÅBO"}),r.a.createElement("p",null,"Such wow. Very React."))}},LbRr:function(e,t,n){"use strict";var a=n("q1tI"),r=n.n(a);t.a=function(e){return r.a.createElement("h1",null,e.headerText)}}}]);
-//# sourceMappingURL=component---src-pages-about-js-8c5f505e11e28177ed44.js.map
